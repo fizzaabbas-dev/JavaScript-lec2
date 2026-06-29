3b8f74a372cd473dd46d0a787993151b17bc7039
fizzaabbas-dev <fizzaabbas.398@gmail.com>
2026-06-29 16:08:29 +0500

    Basic conditions and loops

---
+++ index.js
@@ -0,0 +1,81 @@
+alert("I am working");
+// conditional Statement
+
+let age = 19
+//  19 >=18T
+if(age>=18){
+    console.log("You are eligible to vote");
+
+}
+else{
+    console.log("You are not eligible to vote");
+}
+let provience="Fataa";
+if(province=="Punjab"){
+    console.log("You are from Punjab");
+}
+if(province=="Sindh"){
+    console.log("You are not eligible for this scholarship ")
+
+}
+else if(province=="KPK"){
+    console.log("You are not eligible for this scholarship ")
+}
+if(province=="Balochistan"){
+    console.log("You are  eligible for this scholarship ")
+
+}
+else{
+    console.log("Incomplete information")
+
+}
+
+let grade="A";
+if(grade=="A"){
+console.log("Excelent Result")
+}
+else if(grade=="B"){
+console.log(" Good")
+}
+else if(grade=="C"){
+console.log("Better")
+}
+else if(grade=="D"){
+console.log("Poor")
+}
+else if(grade=="F"){
+console.log("Fail")
+}
+else{
+    console.log("Invalid Grade! Please enter a valid grade.")
+}
+// Loops
+for(let i=1;i<=6;i++){
+    console.log("i")
+    console.log("Fizza ")
+}
+// Even nunmbers
+
+for("i=0;i<=100;i++"){
+    console.log(i)
+}
+for("i=1;i<=100;i++"){
+    console.log(i)
+ }
+//  take user input
+let obtMarks = prompt("Enter your obtained marks");
+let totalMarks= prompt("Enter your total marks");
+
+// typecasting
+obtainMarks = Number(obtMarks);
+totalMarks = Number(totalMarksMarks);
+console.log(typeof obtMarks);
+console.log(typeof totalMarks);
+
+let obtMarks = prompt("Enter your obtained marks");
+let totalMarks= prompt("Enter your total marks");
+let percentage = prompt(obtainMarks/totalMarks)*100
+console.log(percentage);
+console.log(typeofpercentage);
+
+
